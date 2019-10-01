d25a920498ba14eb80ca17601e70ab897c958067
Franz Holzinger <holzinger@ttproducts.de>
2019-10-01 13:49:33 +0200

    ext_emconf.php as it is downloaded from TER

--- ext_emconf.php
+++ ext_emconf.php
@@ -1,54 +1,39 @@
 <?php
 
-/***************************************************************
- * Extension Manager/Repository config file for ext "macina_searchbox".
- *
- * Auto generated 07-08-2013 11:10
- *
- * Manual updates:
- * Only the data in the array - everything else is removed by next
- * writing. "version" and "dependencies" must not be touched!
- ***************************************************************/
+########################################################################
+# Extension Manager/Repository config file for ext "macina_searchbox".
+#
+# Auto generated 08-08-2015 09:19
+#
+# Manual updates:
+# Only the data in the array - everything else is removed by next
+# writing. "version" and "dependencies" must not be touched!
+########################################################################
 
-$EM_CONF[$_EXTKEY] = array (
+$EM_CONF[$_EXTKEY] = array(
 	'title' => 'Searchbox for Indexed Search Engine',
-	'description' => 'This Extension offers the possibility to add a searchbox to your Typo3 powered website which submits the searchvalue to the Indexed Search Engine	plugin. The extension has a html template which is easy to modify and supports multiple languages.',
+	'description' => 'This Extension offers the possibility to add a searchbox to your Typo3 powered website which submits the searchvalue to the Indexed Search Engine  plugin. The extension has a html template which is easy to modify and supports multiple languages.',
 	'category' => 'plugin',
-	'shy' => '0',
+	'shy' => 0,
 	'dependencies' => '',
 	'conflicts' => '',
 	'priority' => '',
 	'loadOrder' => '',
+	'TYPO3_version' => '',
+	'PHP_version' => '',
 	'module' => '',
 	'state' => 'stable',
-	'uploadfolder' => '0',
+	'uploadfolder' => 0,
 	'createDirs' => '',
 	'modify_tables' => '',
-	'clearCacheOnLoad' => '1',
+	'clearCacheOnLoad' => 1,
 	'lockType' => '',
 	'author' => 'Wolfgang Becker',
 	'author_email' => 'wb@macina.com',
 	'author_company' => '',
-	'CGLcompliance' => NULL,
-	'CGLcompliance_note' => NULL,
+	'CGLcompliance' => '',
+	'CGLcompliance_note' => '',
 	'version' => '2.2.0',
-	'constraints' => 
-	array (
-		'depends' => 
-		array (
-			'typo3' => '',
-			'php' => '',
-			'cms' => '',
-			'indexed_search' => '',
-		),
-		'conflicts' => 
-		array (
-			'' => '',
-		),
-		'suggests' => 
-		array (
-		),
-	),
 );
 
 ?>
