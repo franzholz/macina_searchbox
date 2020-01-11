ee7692f8d7e9888a57d120c2ba923a0c817038ca
Franz Holzinger <holzinger@ttproducts.de>
2020-01-11 22:59:44 +0100

    * add the Resources from Indexed Search TYPO3 8.7.27 * renew the configuration: setup.txt lib.macina_searchbox * add a constants.txt

--- ext_emconf.php
+++ ext_emconf.php
@@ -18,11 +18,11 @@ $EM_CONF[$_EXTKEY] = array(
     'author_company' => 'jambage.com',
     'CGLcompliance' => '',
     'CGLcompliance_note' => '',
-    'version' => '2.3.0',
+    'version' => '3.0.0',
     'constraints' => array(
         'depends' => array(
-            'php' => '5.5.0-7.99.99',
-            'typo3' => '7.6.0-8.7.99',
+            'php' => '5.5.0-7.3.99',
+            'typo3' => '7.6.0-9.5.99',
         ),
         'suggests' => array(
             'indexed_search' => '7.6.0-8.7.99',
--- ext_localconf.php
+++ ext_localconf.php
@@ -5,5 +5,4 @@ if (!defined ('MACINA_SEARCHBOX_EXT')) {
     define('MACINA_SEARCHBOX_EXT', 'macina_searchbox');
 }
 
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPItoST43(MACINA_SEARCHBOX_EXT, 'pi1/class.tx_macinasearchbox_pi1.php', '_pi1', 'list_type', 1);
 
