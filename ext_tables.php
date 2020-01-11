29fa05610ae397ba0d3c0e6242a243030156b90f
Franz Holzinger <holzinger@ttproducts.de>
2020-01-11 21:21:53 +0100

    * remove the wizard and remove the plugin because the search box entries did not work together with the Indexed Search controller.

--- ext_tables.php
+++
@@ -1,9 +0,0 @@
-<?php
-defined('TYPO3_MODE') || die('Access denied.');
-
-if (
-    TYPO3_MODE == 'BE'
-) {
-    $GLOBALS['TBE_MODULES_EXT']['xMOD_db_new_content_el']['addElClasses']['JambageCom\\MacinaSearchbox\\Controller\\Plugin\\WizardIcon'] = \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::extPath(MACINA_SEARCHBOX_EXT) . 'Classes/Controller/Plugin/WizardIcon.php';
-}
-
