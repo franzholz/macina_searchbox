b37547b250211189b2335fa50c987ebf581e52b6
Franz Holzinger <holzinger@ttproducts.de>
2021-04-16 13:58:02 +0200

    * bugfix labels in flexform * It should work with TYPO3 10.

--- ext_emconf.php
+++ ext_emconf.php
@@ -9,8 +9,6 @@ $EM_CONF[$_EXTKEY] = array(
     'description' => 'This Extension offers the possibility to add a searchbox to your Typo3 powered website which submits the searchvalue to the Indexed Search Engine  plugin without any result list output. The extension has a Fluid template which is easy to modify.',
     'category' => 'plugin',
     'state' => 'stable',
-    'uploadfolder' => 0,
-    'createDirs' => '',
     'clearCacheOnLoad' => 1,
     'author' => 'Franz Holzinger, Wolfgang Becker',
     'author_email' => 'franz@ttproducts.de',
@@ -18,11 +16,11 @@ $EM_CONF[$_EXTKEY] = array(
     'version' => '3.0.5',
     'constraints' => array(
         'depends' => array(
-            'php' => '5.5.0-7.3.99',
-            'typo3' => '7.6.0-9.5.99',
+            'php' => '5.5.0-7.4.99',
+            'typo3' => '7.6.0-10.4.99',
         ),
         'suggests' => array(
-            'indexed_search' => '7.6.0-9.5.99',
+            'indexed_search' => '7.6.0-10.4.99',
         ),
     ),
 );
