63088e496d086c3843cb6e64d4cb0ded5aebe355
Franz Holzinger <holzinger@ttproducts.de>
2021-05-19 11:57:08 +0200

    bugfix in composer

--- ext_emconf.php
+++ ext_emconf.php
@@ -4,7 +4,7 @@
 # Extension Manager/Repository config file for ext "macina_searchbox".
 ########################################################################
 
-$EM_CONF[$_EXTKEY] = array(
+$EM_CONF[$_EXTKEY] = [
     'title' => 'Searchbox for Indexed Search Engine',
     'description' => 'This Extension offers the possibility to add a searchbox to your Typo3 powered website which submits the searchvalue to the Indexed Search Engine  plugin without any result list output. The extension has a Fluid template which is easy to modify.',
     'category' => 'plugin',
@@ -13,14 +13,14 @@ $EM_CONF[$_EXTKEY] = array(
     'author' => 'Franz Holzinger, Wolfgang Becker',
     'author_email' => 'franz@ttproducts.de',
     'author_company' => 'jambage.com',
-    'version' => '3.0.5',
-    'constraints' => array(
-        'depends' => array(
+    'version' => '3.0.6',
+    'constraints' => [
+        'depends' => [
             'php' => '5.5.0-7.4.99',
             'typo3' => '7.6.0-10.4.99',
-        ),
-        'suggests' => array(
+        ],
+        'suggests' => [
             'indexed_search' => '7.6.0-10.4.99',
-        ),
-    ),
-);
+        ],
+    ],
+];
