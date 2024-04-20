851e8e4982ab3928b4e3d97d5690b2342ddcffe5
Franz Holzinger <franz.holzinger@festwein.de>
2024-04-20 20:10:35 +0200

    Add empty plugin with help text and others

---
+++ Classes/Controller/RegisterPluginController.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace JambageCom\MacinaSearchbox\Controller;
+
+/*
+ * This file is part of the TYPO3 CMS project.
+ *
+ * It is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License, either version 2
+ * of the License, or any later version.
+ *
+ * For the full copyright and license information, please read the
+ * LICENSE.txt file that was distributed with this source code.
+ *
+ * The TYPO3 project - inspiring people to share!
+ */
+
+use TYPO3\CMS\Frontend\Plugin\AbstractPlugin;
+
+
+class RegisterPluginController extends AbstractPlugin
+{
+    /**
+     * Should normally be set in the main function with the TypoScript content passed to the method.
+     *
+     * $conf['LOCAL_LANG'][_key_] is reserved for Local Language overrides.
+     * $conf['CODE.']['10.']['userFunc'] reserved for setting up the USER / USER_INT object. See TSref where CODE is the code of the plugin
+     *
+     * @var array
+     */
+    public $conf = [];
+
+    public $extensionKey = 'macina_searchbox';
+
+
+    /**
+    * Main board function. Call this from TypoScript
+    */
+    public function main($content, $conf)
+    {
+        $content = 'The extension ' . $this->extensionKey . ' does not use its own plugin but the setup lib.macina_searchbox to have your search plugin on one or many pages at the same time. The search results are forwarded to the TYPO3 system extension Indexed Search. This gives you the possibility for an easier insertion of search mask on each page at once.';
+        return $content;
+    }
+}
--- Configuration/TCA/Overrides/tt_content.php
+++ Configuration/TCA/Overrides/tt_content.php
@@ -1,24 +1,27 @@
 <?php
 defined('TYPO3_MODE') || die('Access denied.');
 
+use \TYPO3\CMS\Core\Utility\ExtensionManagementUtility;
+
 call_user_func(function ($extensionKey, $table): void {
     $listType = $extensionKey . '_pi1';
+    $languageSubpath = '/Resources/Private/Language/';
 
     $GLOBALS['TCA'][$table]['types']['list']['subtypes_excludelist'][$listType] = 'layout';
     $GLOBALS['TCA'][$table]['types']['list']['subtypes_addlist'][$listType] = 'pi_flexform';
 
-    \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPiFlexFormValue(
+    ExtensionManagementUtility::addPiFlexFormValue(
         $listType,
         'FILE:EXT:' . $extensionKey . '/Configuration/Flexforms/Plugin.xml'
     );
 
-    \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPlugin(
+    ExtensionManagementUtility::addPlugin(
         [
-            'LLL:EXT:' . $extensionKey . '/Resources/Private/Language/locallang_db.xlf:tt_content.list_type',
-            $listType,
-            'EXT:' . $extensionKey . '/ext_icon.gif'
+            'LLL:EXT:' . $extensionKey . $languageSubpath . 'locallang_db.xlf:tt_content.list_type',
+            $listType
         ],
         'list_type',
         $extensionKey
     );
 }, 'macina_searchbox', basename(__FILE__, '.php'));
+
