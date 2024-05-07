1c1df54f01e1fb21e46b842c71d16d8796a8de2d
Franz Holzinger <franz.holzinger@festwein.de>
2024-05-07 12:05:35 +0200

    fix #5: use constant TYPO3 instead of TYPO3_MODE

--- Configuration/TCA/Overrides/sys_template.php
+++ Configuration/TCA/Overrides/sys_template.php
@@ -1,8 +1,8 @@
 <?php
-defined('TYPO3_MODE') || die('Access denied.');
+defined('TYPO3') || die('Access denied.');
 
 call_user_func(function ($extensionKey, $table): void {
-    
+
     \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile(
         $extensionKey,
         'Configuration/TypoScript/PluginSetup/',
--- ext_localconf.php
+++ ext_localconf.php
@@ -1,4 +1,4 @@
 <?php
-defined('TYPO3_MODE') || die('Access denied.');
+defined('TYPO3') || die('Access denied.');
 
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPItoST43('macina_searchbox', '', '_pi1');
